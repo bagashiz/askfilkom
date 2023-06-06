1f6bf1108e96fdbd31a6fd9eb90380b9a64c1def
Reyhan Prasetyo <prasetyoreyhan0509@gmail.com>
2023-06-06 22:19:54 +0700

    feat(views): add pertanyaan views

--- resources/views/pertanyaan/index.blade.php
+++ resources/views/pertanyaan/index.blade.php
@@ -1,106 +1,77 @@
 <x-layout>
 
-    <!-- Search -->
-    <section class="my-6">
-        <form>
-            <div class="grid gap-6 mb-6">
-                <div class=" mx-auto w-2/4">
-                    <input type="text" id="first_name"
-                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 pl-6"
-                        placeholder="Cari pertanyaan serta topik disini" required>
-                </div>
-            </div>
-        </form>
+    {{-- Search --}}
+    <section class="mt-4">
+        @include('partials._search')
     </section>
 
-    <!-- Filter -->
-    <section class="w-10/12 mx-auto my-2">
-        <div class="ml-8 lex space-x-2 grid-cols-3">
-            <p style="display: inline-block; vertical-align: middle;">Urutkan berdasarkan :</p>
-            <button type="button"
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-auto px-3 py-2 text-center"
-                style="display: inline-block; vertical-align: middle;">Terpopuler</button>
+    {{-- Filter --}}
+    <section class="w-10/12 mx-6">
+        <div class="lex space-x-2 grid-cols-3">
+            <p class="w-auto text-sm px-3 py-2">Urutkan Berdasarkan: </p>
+            {{-- Filter by newest or most votes --}}
+            <a href="{{ request()->fullUrlWithQuery(['sort' => 'newest']) }}" class="inline-block">
+                <button type="button"
+                    class="text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-xs px-3 py-2">
+                    Terbaru
+                </button>
+            </a>
+            <a href="{{ request()->fullUrlWithQuery(['sort' => 'votes']) }}" class="inline-block">
+                <button type="button"
+                    class="text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-xs px-3 py-2">
+                    Terpopuler
+                </button>
+            </a>
         </div>
     </section>
 
-    
-    <!-- Tabel -->
-    <section class="class=w-10/12 mx-auto my-4">
-        <div class="relative overflow-x-auto ml-8 lex space-x-2">
-            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+
+    {{-- Pertanyaan table --}}
+    <section class="mx-auto my-4">
+        <div class="relative overflow-x-auto mx-8 lex space-x-2">
+            <table class="md:table-fixed w-full text-sm text-left text-gray-500 dark:text-gray-400">
                 <thead class="text-xs text-gray-700 normal-case bg-gray-50">
                     <tr>
-                        <th scope="col" class="px-6 py-3 text-lg">
+                        <th scope="col" class="px-6 py-3 text-lg text-center w-5/12">
                             Pertanyaan
                         </th>
-                        <th scope="col" class="px-6 py-3 text-lg">
+                        <th scope="col" class="px-6 py-3 text-lg text-center w-2/12">
+                            Topik
+                        </th>
+                        <th scope="col" class="px-6 py-3 text-lg text-center w-2/12">
                             Asker
                         </th>
-                        <th scope="col" class="px-6 py-3 text-lg">
-                            Jumlah Vote
+                        <th scope="col" class="px-6 py-3 text-lg text-center w-1/12">
+                            Votes
                         </th>
-                        <th scope="col" class="px-6 py-3 text-lg">
-                            Created At
+
+                        <th scope="col" class="px-6 py-3 text-lg text-center w-1/12">
+                            Jawaban
+                        </th>
+                        <th scope="col" class="px-6 py-3 text-lg text-center">
+                            Tanggal
                         </th>
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach ($pertanyaan as $item)
-                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                        <td scope="row"
-                            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                                <a href="/pertanyaan/{{$item->id_pertanyaan}}">
-                                    {{$item->judul}}
-                                </a>
+                    @unless ($pertanyaan->isEmpty())
+                        @foreach ($pertanyaan as $item)
+                            <x-pertanyaan-row :pertanyaan="$item" />
+                        @endforeach
+                    @else
+                        <tr class="border-gray-300">
+                            <td class="px-4 py-3 border-t border-b border-gray-300 text-lg text-center" colspan="5">
+                                Tidak ada pertanyaan yang tersedia.
                             </td>
-                        <td class="px-6 py-4">
-                            <a href="/users/{{$item->user->id_user}}">
-                                {{$item->user->username}}
-                            </a>
-                        </td>
-                        <td class="px-6 py-4">
-                            {{$item->jumlah_vote}}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{$item->created_at}}
-                        </td>
-                    </tr>
-                    @endforeach
+                        </tr>
+                    @endunless
                 </tbody>
             </table>
         </div>
-
         <br>
-
+        {{-- Pagination --}}
         <div class="flex justify-center">
-            <p>{{$pertanyaan}}</p>
+            <p>{{ $pertanyaan->links() }}</p>
         </div>
-
-        {{-- <div aria-label="Page navigation" class="flex justify-center my-6">
-            <ul class="inline-flex -space-x-px">
-                <li>
-                    <a href="#"
-                        class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">1</a>
-                </li>
-                <li>
-                    <a href="#"
-                        class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</a>
-                </li>
-                <li>
-                    <a href="#" aria-current="page"
-                    class="px-3 py-2 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</a>
-                </li>
-                <li>
-                    <a href="#"
-                    class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">4</a>
-                </li>
-                <li>
-                    <a href="#"
-                        class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">5</a>
-                </li>
-            </ul>
-        </div> --}}
-        
     </section>
-
 </x-layout>
--- resources/views/pertanyaan/show.blade.php
+++ resources/views/pertanyaan/show.blade.php
@@ -1,81 +1,86 @@
 <x-layout>
-
-    <section class="w-10/12 mx-auto mb-24">
-
-        {{-- judul & topik --}}
-        <div class="container mx-6 text-start justify-center my-6">
-            <div class="">
-                <p class="text-start font-semibold text-2xl mx-0">{{ $pertanyaan->judul }}</p>
-                <div class="justify-start flex flex-wrap items-center text-start">
+    <section class="container mx-auto px-4">
+        {{-- Pertanyaan --}}
+        <section class="container mx-auto my-6">
+            <div class="flex justify-between">
+                <div>
+                    <p class="font-bold text-3xl mr-4 mb-4">{{ $pertanyaan->judul }}</p>
                     @foreach ($pertanyaan->topik as $item)
-                        <p class="mr-2">{{ $item->nama }}</p>
+                        <x-topik-button :topik="$item" />
                     @endforeach
                 </div>
+                <div class="text-right">
+                    <p class="text-sm text-gray-500">
+                        Ditanyakan oleh
+                        <span class="font-bold">
+                            @if (auth()->id() === $pertanyaan->user->id_user)
+                                <a href="/profile" class="text-gray-500 hover:text-blue-500 hover:underline">
+                                @else
+                                    <a href="/users/{{ $pertanyaan->user->id_user }}"
+                                        class="text-gray-500 hover:text-blue-500 hover:underline">
+                            @endif
+                            {{ $pertanyaan->user->username }}
+                            </a>
+                        </span>
+                    </p>
+                    <p class="text-sm text-gray-500">{{ $pertanyaan->created_at }}</p>
+                    @auth
+                        @if (auth()->id() === $pertanyaan->user->id_user || auth()->user()->is_admin)
+                            @if (auth()->id() === $pertanyaan->user->id_user)
+                                <div class="mt-2">
+                                    <a href="/pertanyaan/{{ $pertanyaan->id_pertanyaan }}/edit"
+                                        class="text-gray-500 hover:text-blue-500 mx-1">
+                                        <i class="far fa-edit"></i>
+                                    </a>
+                            @endif
+                            <form action="/pertanyaan/{{ $pertanyaan->id_pertanyaan }}" method="POST" class="inline">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="text-red-500 hover:text-red-700 mx-1">
+                                    <i class="far fa-trash-alt"></i>
+                                </button>
+                            </form>
+                    </div>
+                    @endif
+                @endauth
             </div>
-        </div>
-
-        {{-- isi post --}}
-        <div class="container mx-6 mt-4">
-
-            {{-- nama & tanggal post --}}
-            <div class="justify-start max-w-screen-xl flex flex-wrap items-center text-start">
-                <p class="mr-2">{{ $pertanyaan->user->nama }}</p>
-                <p class="ml-2">{{ $pertanyaan->created_at }}</p>
             </div>
-
-            {{-- isi --}}
-            <p class="max-w-screen-xl text-justify mt-2">
-                {{ $pertanyaan->deskripsi }}
+            <p class="max-w-screen-xl text-justify my-2">
+                {!! nl2br(e($pertanyaan->deskripsi)) !!}
             </p>
-        </div>
+        </section>
 
-        <br>
-
-        {{-- upvote --}}
-        <div class="justify-start max-w-screen-xl flex flex-wrap items-center text-start">
-            <button type="button"
-                class="mx-6 py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 text-center inline-flex items-center">
-                <i class="fa-regular fa-thumbs-up pr-2"></i>
-                Dukung Naik
-            </button>
-            <p class="text-2xl mx-4"> • </p>
-            <p class="ml-2 font-semibold">201</p>
-        </div>
-
-        {{-- input field komentar --}}
-        <form action="/jawaban" method="POST">
-            @csrf
-            <div class="my-4 mx-6">
-                <input type="hidden" name="id_user" value="{{ auth()->id() }}">
-                <input type="hidden" name="id_pertanyaan" value="{{ $pertanyaan->id_pertanyaan }}">
-                <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Default
-                    input</label>
-                <input type="text" id="default-input" name="deskripsi"
-                    class="placeholderbg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600"
-                    placeholder="Tambahkan komentar ...">
+        {{-- Vote button --}}
+        <section class="container mx-auto my-5">
+            <div class="justify-start max-w-screen-xl">
+                <x-vote-button :votes="$pertanyaan->jumlah_vote" :voted="$pertanyaan->hasVotedByUser(auth()->user())" postId="{{ $pertanyaan->id_pertanyaan }}"
+                    postType="pertanyaan" />
             </div>
+        </section>
 
-            <div class="flex justify-end">
-                <button type="submit"
-                    class="mr-6 my-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2">submit</button>
-            </div>
-        </form>
+        {{-- Jawaban form --}}
+        <section class="container mx-auto">
+            <form action="/jawaban" method="POST" class="flex items-center">
+                @csrf
+                <div class="mx-auto w-full flex justify-center items-center">
+                    <input type="hidden" name="id_user" value="{{ auth()->id() }}">
+                    <input type="hidden" name="id_pertanyaan" value="{{ $pertanyaan->id_pertanyaan }}">
+                    <textarea id="default-input" name="deskripsi"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-l-full focus:ring-blue-500 focus:border-blue-500 p-2.5 w-full h-10 resize-none whitespace-pre-wrap dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="apa tanggapanmu?" required=""></textarea>
+                    <button type="submit"
+                        class="bg-blue-500 text-white py-2 px-4 rounded-r-full hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300">
+                        Unggah
+                    </button>
+                </div>
+            </form>
+        </section>
 
-        {{-- komentar --}}
-        <div class="container mx-6">
+        {{-- Jawaban --}}
+        <section class="container mx-auto my-6">
             @foreach ($pertanyaan->jawaban as $item)
-                
-            <div class="justify-start max-w-screen-xl flex flex-wrap items-center text-start">
-                <p class="mr-2">{{$item->user->username}}</p>
-                <p class="ml-2">{{$item->created_at}}</p>
-            </div>
-            <p class="max-w-screen-xl text-justify mt-2 mb-6">
-                {{$item->deskripsi}}
-            </p>
-
+                <x-jawaban-card :jawaban="$item" />
             @endforeach
-        </div>
-
+        </section>
     </section>
-
 </x-layout>
